714a019a5741fa3a7e6100763fc35fa55adbc9fa
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-08-09 15:46:21 -0300

    front-aula1

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Container>  
+        <MenuBar />
+        <Route exact path='/' component={Home}/>
+        <Route exact path= '/login' component={Login}/>
+        <Route exact path= '/register' component={Register}/>
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+ 
